a6c1f85dcc9e2d6b1ecadd8e96cc68a9cc732fdf
dsvivass <dsvivass@unal.edu.co>
2023-10-13 20:01:16 -0500

    Basic Setup

---
+++ app/about.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native"
+import { Link } from 'expo-router'
+
+export default function Page() {
+    return <View>
+        <Text>About Textttttt</Text>
+        <Link href="/">Home</Link>
+    </View>
+}
